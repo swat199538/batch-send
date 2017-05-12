ccacb63b00db793beb78062f017715849ca0ab84
wangliang <wangliang199538@live.com>
2017-05-12 18:01:08 +0800

    修改了目录结构

--- app/Http/Controllers/SmsAssistant/IndexController.php
+++ app/Http/Controllers/SmsAssistant/IndexController.php
@@ -3,15 +3,21 @@
 namespace App\Http\Controllers\SmsAssistant;
 
 use App\Http\Controllers\Controller;
+use App\Model\category;
 use Illuminate\Http\Request;
+use Mockery\Exception;
 
 class IndexController extends Controller
 {
 
     //短信群发页面
-    public function index()
+    public function index(Request $request, category $category, $id)
     {
-        return view('tool.groupSend');
+        $smsInfo = $category->getSmsTempleById($id);
+        if ($smsInfo == null){
+            return '没有数据';
+        }
+        echo $smsInfo->name;
     }
 
 }
