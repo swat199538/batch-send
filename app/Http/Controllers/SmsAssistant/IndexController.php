6f3e10f2f5c38a861136cf592e91a2c40a280e9a
wangliang <wangliang199538@live.com>
2017-05-27 09:49:46 +0800

    set cookie domain

--- app/Http/Controllers/SmsAssistant/IndexController.php
+++ app/Http/Controllers/SmsAssistant/IndexController.php
@@ -34,7 +34,7 @@ class IndexController extends Controller
         $template = $assistantTemple->getTempleByCategory($TempleInfo->category_id, 1);
         $unsent = $assistantSubmitLog->getUnsentLogByUuid($this->uuid);
         $unsentCount =count($unsent);
-        setcookie('unsent', $unsentCount, time()+3600*168, '/');
+        setcookie('unsent', $unsentCount, time()+3600*168, '.smsbao.com');
         $assistantTemple->increment('click_count');
         return $view = view('tool.groupSend')->with([
             'TempleInfo'=>$TempleInfo,
@@ -111,7 +111,7 @@ class IndexController extends Controller
             $info2 = $assistantSubmitLog->getLogInfoByuuid($this->uuid);
             $unsent = $assistantSubmitLog->getUnsentLogByUuid($this->uuid);
             $unsentCount =count($unsent);
-            setcookie('unsent', $unsentCount, time()+3600*168, '/');
+            setcookie('unsent', $unsentCount, time()+3600*168, '.smsbao.com');
 
             return view('tool.importSend')->with([
                 'TempleInfo'=>$data,
